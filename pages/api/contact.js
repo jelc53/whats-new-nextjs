75b0e2f0f9c120f34c09f138f5b8e02fbe3d9187
Julian Cooper <julian.e.cooper2@gmail.com>
2023-09-26 08:28:20 +0200

    bug fix: contact form for production environment

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -45,7 +45,7 @@ export default async function ContactAPI(req, res) {
 
     await new Promise((resolve, reject) => {
         // send mail
-        transporter.sendMail(mailData, function (err, info) => {
+        transporter.sendMail(mailData, function (err, info) {
             if (err) {
                 console.error(err);
                 reject(err);

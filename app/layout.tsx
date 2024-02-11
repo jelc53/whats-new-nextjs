66736dc67fe7692b0f7855232d7aa22e1db03e92
Julian Cooper <julian.e.cooper2@gmail.com>
2024-02-11 21:40:44 +0100

    enabled inline mathjax

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,10 +9,23 @@ export default function RootLayout({
 }: {
   children: React.ReactNode
 }) {
-
+  
   return (
     <html lang="en">
       <head>
+        <script
+          type="text/x-mathjax-config"
+          dangerouslySetInnerHTML={{
+            __html: `
+              MathJax.Hub.Config({
+                tex2jax: {
+                  inlineMath: [ ['$','$'] ],
+                  processEscapes: true
+                }
+              });
+            `,
+          }}
+        />
         <script type="text/javascript" async src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.7/MathJax.js?config=TeX-MML-AM_CHTML"></script>
       </head>
       <body className="flex flex-col justify-between h-screen dark:bg-stone-900">

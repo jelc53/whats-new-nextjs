6b2513e4da8635dcaf21a9fb1619b744d9c4a6aa
Cooper-Julian <julian.e.cooper2@gmail.com>
2024-02-11 21:44:54 +0100

    bug fix for mathjax inline latex

--- app/layout.tsx
+++ app/layout.tsx
@@ -13,6 +13,19 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <script
+          type="text/x-mathjax-config"
+          dangerouslySetInnerHTML={{
+            __html: `
+              MathJax.Hub.Config({
+                tex2jax: {
+                  inlineMath: [ ['$','$'], ["\\(","\\)"] ],
+                  processEscapes: true
+                }
+              });
+            `,
+          }}
+        />
         <script type="text/javascript" async src="https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.7/MathJax.js?config=TeX-MML-AM_CHTML"></script>
       </head>
       <body className="flex flex-col justify-between h-screen dark:bg-stone-900">
